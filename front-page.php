31cefb32f4f15fc43d9cf3a3909a1301febde60d
Matt Calvert <xander.raymatt@gmail.com>
2023-08-15 22:26:57 +0100

    Screenshot and front-page.php

---
+++ front-page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Wedding_Theme
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>We're getting<br>married</h1>
+                </div>
+            </div>
+        </div>
+    <!-- bg image here -->
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
